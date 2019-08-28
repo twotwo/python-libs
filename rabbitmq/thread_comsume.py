b20a1cd2cfafba9b3634ef391c21af2bc5d3565c
li3huo <twotwo.li@gmail.com>
2019-08-28 13:50:44 +0800

    [MOD] RabbitMQ Code Sample
    refine code on thread_comsume.py

--- rabbitmq/thread_comsume.py
+++ rabbitmq/thread_comsume.py
@@ -5,38 +5,38 @@
 #
 ############################################################
 import os
+import time
 import pika
+from pika.exceptions import AMQPConnectionError
 from loguru import logger
 import multiprocessing
 # add to make multiprocessing work (this way multiprocessing will not use fork).
 multiprocessing.set_start_method('spawn', True)
 
 
-def emit_log(message):
+def get_connection():
+    logger.warning(f"connet to {os.getenv('RABBITMQ_HOST', 'dev.gpu')}")
+    return pika.BlockingConnection(pika.ConnectionParameters(os.getenv(
+        'RABBITMQ_HOST', 'dev.gpu'), credentials=pika.PlainCredentials('guest', os.getenv('RABBITMQ_PASS', 'guest'))))
+
+
+def emit_log(channel, message):
     try:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(
-            'dev.gpu', credentials=pika.PlainCredentials('guest', os.getenv("RABBITMQ_PASS", 'guest'))))
-        channel = connection.channel()
-
-        logger.debug("create exchange ...")
-        channel.exchange_declare(exchange='logs', exchange_type='topic')
-
         logger.debug("sending ...")
         # channel.basic_publish(exchange='', routing_key='hello', body=message)
-        channel.basic_publish(exchange='logs', routing_key='', body=message)
+        channel.basic_publish(
+            exchange='logs', routing_key='logs.test', body=message)
 
         logger.info("[x] Sent '%s'" % message)
-        connection.close()
     except Exception as ex:
         raise ex
 
 
-def receive_logs():
+def receive_logs(connection):
     logger.debug("ENTERED")
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='dev.gpu'))
-    # connection = pika.BlockingConnection(pika.ConnectionParameters(
-    #     'dev.gpu', credentials=pika.PlainCredentials('guest', os.getenv("RABBITMQ_PASS", 'guest'))))
+    # connection = pika.BlockingConnection(
+    #     pika.ConnectionParameters(host='dev.gpu'))
+    connection = get_connection()
     logger.debug("1")
     channel = connection.channel()
     logger.debug("2")
@@ -52,23 +52,37 @@ def receive_logs():
 
     logger.debug("6")
     channel.queue_bind(exchange='logs', queue=queue_name,
-                       routing_key='test.routing.key')
+                       routing_key='#')
     logger.debug("7")
-    channel.basic_consume(callback_rabbit, queue=queue_name, no_ack=True)
-    logger.debug("8")
-    channel.start_consuming()
+    # channel.basic_consume(callback_rabbit, queue=queue_name, no_ack=True)
+    # logger.debug("8")
+    # channel.start_consuming()
+    try:
+        for method, properties, body in channel.consume(queue=queue_name, no_ack=True):
+            callback_rabbit(channel, method, properties, body)
+    except AMQPConnectionError as er:
+        logger.error(f'channel.consume failed, er: {er}')
+    except KeyboardInterrupt:
+        logger.info('exit ...')
 
 
 def start():
-    # t_msg = threading.Thread(target=receive_logs)
-    t_msg = multiprocessing.Process(target=receive_logs, args=())
+    connection = get_connection()
+    connection = None
+    t_msg = multiprocessing.Process(target=receive_logs, args=(connection, ))
     t_msg.start()
     t_msg.join(0)
 
 
 if __name__ == '__main__':
-    # receive_logs()
     start()
+    time.sleep(1)
+
+    connection = get_connection()
+    channel = connection.channel()
+
+    logger.debug("create exchange ...")
+    channel.exchange_declare(exchange='logs', exchange_type='topic')
     logger.warning('begin send')
     for i in range(10):
-        emit_log(f'[{i}] hello')
+        emit_log(channel, f'[{i}] hello')
