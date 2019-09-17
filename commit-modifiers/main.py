3154e5ad07f57a91f3c7199e649560681a875acd
li3huo <twotwo.li@gmail.com>
2019-09-17 10:09:53 +0800

    [MOD] Git Commit Modifiers

--- commit-modifiers/main.py
+++ commit-modifiers/main.py
@@ -23,16 +23,16 @@ class GitModifier(object):
     def __init__(self, repo_path):
         self.repo = Repo(repo_path)
 
-    @classmethod
-    def format(cls, obj):
-        template = """'
-        if  test "$GIT_COMMIT" = "{commit_id}"
+    @staticmethod
+    def format(obj):
+        template = """
+        if test "$GIT_COMMIT" = "{commit_id}"
         then
-            export GIT_AUTHOR_DATE="{date}"
-            export GIT_COMMITTER_DATE="{date}"
-            export GIT_AUTHOR_NAME="{user}"
-            export GIT_AUTHOR_EMAIL="{email}"
-        fi'
+            GIT_AUTHOR_DATE="{date}"
+            GIT_COMMITTER_DATE="{date}"
+            GIT_AUTHOR_NAME="{user}"
+            GIT_AUTHOR_EMAIL="{email}"
+        fi
         """
 
         return template.format(commit_id=obj.get('id'),
@@ -40,49 +40,42 @@ class GitModifier(object):
                                user=obj.get('author').get('name'),
                                email=obj.get('author').get('email'))
 
-    @classmethod
-    def to_timestamp(cls, date):
-        return (date - datetime.datetime(1970, 1, 1)).total_seconds()
+    @staticmethod
+    def filter_branch(path, msg, verbose=False):
+        """
+        https://git-scm.com/docs/git-filter-branch
 
-    def get_commits(self, branch_name, max_count=50, begin_date=None, end_date=None):
-        if end_date is None:
-            end_date = datetime.datetime.now()
+        """
+        cmd = f"git -C {path} filter-branch -f --env-filter {msg}"
+        if verbose:
+            event_logger.info(f"executing...\n{cmd}")
+        return GitModifier.execute(cmd)
 
-        if begin_date is None:
-            begin_date = end_date - datetime.timedelta(days=30)
-
-        min_age = self.to_timestamp(end_date)
-        max_age = self.to_timestamp(begin_date)
-        commmits = list(self.repo.iter_commits(
-            branch_name, max_count=max_count, max_age=max_age, min_age=min_age))
-
-        cms = []
-        for commit in commmits:
-            cms.append(dict(
-                commit_id=commit.hexsha,
-                author_name=commit.author.name,
-                author_date=datetime.datetime.fromtimestamp(
-                    commit.authored_date).strftime('%Y-%m-%d %H:%M:%S'),
-                commit_date=datetime.datetime.fromtimestamp(
-                    commit.committed_date).strftime('%Y-%m-%d %H:%M:%S'),
-                commit_message=commit.message
-            ))
-        return cms
+    @staticmethod
+    def execute(cmd: str) -> str:
+        """Excuete a shell command, get result
+        """
+        with subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True) as proc:
+            lines = proc.stdout.readlines()
+            return '\n'.join([line.decode("utf-8").strip() for line in lines])
 
 
-def execute(cmd: str) -> str:
-    """Excuete a shell command, get result
+def chunks(l: list, n: int):
     """
-    with subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True) as proc:
-        lines = proc.stdout.readlines()
-        return '\n'.join([line.decode("utf-8").strip() for line in lines])
-
+    Yield successive n-sized chunks from l.
+    :param list to devide
+    :n range to devide
+    """
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Console2RabbitMQ')
     parser.add_argument('-p', dest='path', type=str, default="/var/lib/repo")
     parser.add_argument('--export', dest='export', action='store_true')
     parser.add_argument('--modify', dest='modify', action='store_true')
+    parser.add_argument('--verbose', dest='verbose', action='store_true')
+    parser.add_argument('-r', dest='range', type=int, default=5)
     parser.add_argument('-f', dest='file', type=str, default="commits.json")
     args = parser.parse_args()
     # event_logger.warning(f'args={args}')
@@ -106,12 +99,15 @@ if __name__ == '__main__':
         f.close()
         event_logger.opt(ansi=True).info(f'write to {args.file}')
     if(args.modify):
-        # repo = Repo(args.path)
+        envs = []
         with open(args.file) as f:
             for line in f:
                 obj = json.loads(line)
                 event_logger.opt(ansi=True).info(
                     f"<level>{obj.get('id')}</level>\t<cyan>{obj.get('date')}</cyan>\t<blue>{obj.get('author').get('email')}</blue>\t<green>{obj.get('message')}</green>")
-                cmd = f"git -C {args.path} filter-branch -f --env-filter {GitModifier.format(obj)}"
-                # repo.git.filter_branch("-f", "--env-filter \\\n", 'cmd')
-                emit_logger.opt(ansi=True).debug(execute(cmd))
+                envs.append(GitModifier.format(obj))
+        
+        for chunk in chunks(envs, args.range):
+            emit_logger.opt(ansi=True).debug(
+                GitModifier.filter_branch(args.path, f"'{''.join(chunk)}' -- --all",args.verbose))
+        emit_logger.info('finished.')
