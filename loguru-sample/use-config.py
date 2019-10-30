47b2f394d3241afd982c0913577713005c7e7193
li3huo <twotwo.li@gmail.com>
2019-10-30 15:46:33 +0800

    correct spelling

--- loguru-sample/use-config.py
+++ loguru-sample/use-config.py
@@ -11,19 +11,19 @@ from loguru import logger
 logger.configure(
     handlers=[
         dict(sink=sys.stderr, backtrace=False,
-             filter=lambda record: "defaut" in record["extra"]),
+             filter=lambda record: "default" in record["extra"]),
         dict(sink=sys.stdout, backtrace=False,
              format="{message}", level="INFO",
              filter=lambda record: "emitter" in record["extra"]),
         dict(sink="log/{time:YYYY-MM-DD}.log",
-             filter=lambda record: "defaut" in record["extra"],
+             filter=lambda record: "default" in record["extra"],
              backtrace=False, enqueue=True, rotation="10 MB"),
     ],
     patch=lambda record: record["extra"]
 )
 
 statis_logger = logger.bind(emitter=True)
-default_logger = logger.bind(defaut=True)
+default_logger = logger.bind(default=True)
 
 default_logger.info('hello')
 
